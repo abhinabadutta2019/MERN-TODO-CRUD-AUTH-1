b4906380f5600b6926855934541e287def4663e6
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-06 21:12:27 +0530

    mongoDB setup

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,26 @@ const app = express();
 app.use(express.json);
 app.use(cors());
 dotenv.config();
+//
+///////mongoDB cloud//////////////////
+let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.te788iv.mongodb.net/MERN-todo-app?retryWrites=true&w=majority`;
+//
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//////////////////////////////////////////////
+/// this part just to check mongoDB connection////////////////////////////////
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+////////////////////////////
 
 const PORT = process.env.PORT || 3009;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
