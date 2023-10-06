38ab46b6ce3abda3f84d99318ad6e6ff611cc367
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-10-06 22:09:44 +0530

    this structure solved the error

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const UserSchema = new Schema({
+const UserSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
 const UserModel = mongoose.model("user", UserSchema);
 
-export { UserModel };
+export default UserModel;
